fbe0d1ad93bcffe9f08df21c0b348118e927a481
6604062630056 <152253620+6604062630056@users.noreply.github.com>
2024-12-03 13:19:02 +0700

    Commit 3

--- lab1minx.c
+++ lab1minx.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 
 int main() {
-	int a = 49;
-	int b = 33;
+	int a;
+	int b;
+	printf("a=");
+	scanf("%d",&a);
+	printf("b=");
+	scanf("%d",&b);
 	if (a>b) {
-		printf("a more than b");
+		printf("a more than b = %d",a-b);
 	}
 	else {
-		printf("b more than a");
+		printf("b more than a = %d",b-a);
 	}
 }
